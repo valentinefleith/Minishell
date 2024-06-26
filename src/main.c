2483afaa1ed37626925a16856a5aa35240838121
Lucie Vallee <luvallee@made-f0Br8s8.clusters.42paris.fr>
2024-06-26 16:04:54 +0200

    test

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 17:44:12 by vafleith          #+#    #+#             */
-/*   Updated: 2024/06/19 17:45:02 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/06/26 16:04:20 by luvallee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ int main(int argc, char **argv, char **env)
 	(void)argc;
 	(void)argv;
 	(void)env;
+	ft_printf("test\n");
 	ft_printf("compiled!\n");
 }
