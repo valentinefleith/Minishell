96b98a7491dc32417210afa2f56246eb6dd33e01
Lucie <vallee.lucie99@gmail.com>
2024-07-02 17:45:26 +0200

    test pull request +

--- src/builtin/parse_builtin.c
+++ src/builtin/parse_builtin.c
@@ -6,7 +6,7 @@
 /*   By: luvallee <luvallee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 14:36:18 by luvallee          #+#    #+#             */
-/*   Updated: 2024/07/02 15:58:48 by luvallee         ###   ########.fr       */
+/*   Updated: 2024/07/02 16:51:52 by luvallee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,16 +35,13 @@ void	parse_builtin(char **buffer, t_input *input)
 	get_tab_builtin(tab_builtin);
 	if (!buffer)
 		return ;
-	while (i < NONE && buffer[i] != NULL)
+	while (i < NONE )
 	{
 		len = ft_strlen(tab_builtin[i]);
-		if (ft_strncmp(buffer[i], tab_builtin[i], len) == 0)
+		if (ft_strncmp(buffer[0], tab_builtin[i], len) == 0)
 		{
 			input->builtin = i;
-			input->arg = malloc(sizeof(char) * ft_strlen(buffer[i + 1]));
-			if (!input->arg)
-				error_builtin(7);
-			input->arg = buffer[i + 1];
+			input->arg = buffer[1]; // must write a function to parse arguments for builtin
 			break ;
 		}
 		else
