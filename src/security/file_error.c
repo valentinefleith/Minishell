2ffc2ca71639a4d46c50d6dabd7ef2dfd6037e96
valentinefleith <valentine.fleith@gmail.com>
2024-10-02 17:35:53 +0200

    norm

--- src/security/file_error.c
+++ src/security/file_error.c
@@ -6,48 +6,48 @@
 /*   By: luvallee <luvallee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 16:18:26 by vafleith          #+#    #+#             */
-/*   Updated: 2024/09/22 17:56:38 by luvallee         ###   ########.fr       */
+/*   Updated: 2024/10/02 17:35:31 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static bool is_only_slash_dot(char *data)
+static bool	is_only_slash_dot(char *data)
 {
 	while (*data)
 	{
 		if (*data != '/' && *data != '.')
-			return false;
+			return (false);
 		data++;
 	}
-	return true;
+	return (true);
 }
 
-static bool is_only_dot(char *data)
+static bool	is_only_dot(char *data)
 {
 	while (*data)
 	{
 		if (*data != '.')
-			return false;
+			return (false);
 		data++;
 	}
-	return true;
+	return (true);
 }
 
-t_token *check_file_error(t_token *tokens, t_env *envs)
+t_token	*check_file_error(t_token *tokens, t_env *envs)
 {
 	if (!tokens || !tokens->data)
-		return tokens;
+		return (tokens);
 	if (ft_strlen(tokens->data) == 1 && tokens->data[0] == '.')
 	{
 		ft_putendl_fd("bash: .: filename argument required", 2);
 		ft_free_tokens(tokens);
 		tokens = NULL;
 		update_exit_status(envs->env_list, 2);
-		return tokens;
+		return (tokens);
 	}
 	if (is_only_dot(tokens->data))
-		return tokens;
+		return (tokens);
 	if (is_only_slash_dot(tokens->data))
 	{
 		ft_putstr_fd("bash: ", 2);
@@ -56,7 +56,7 @@ t_token *check_file_error(t_token *tokens, t_env *envs)
 		ft_free_tokens(tokens);
 		tokens = NULL;
 		update_exit_status(envs->env_list, 126);
-		return tokens;
+		return (tokens);
 	}
-	return tokens;
+	return (tokens);
 }
