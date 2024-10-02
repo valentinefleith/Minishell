b12625b052d45cad3799ab4fd6ebd0a23cd8323b
valentinefleith <valentine.fleith@gmail.com>
2024-10-02 22:19:56 +0200

    format env

--- src/env/env_utils.c
+++ src/env/env_utils.c
@@ -6,7 +6,7 @@
 /*   By: luvallee <luvallee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 14:46:11 by luvallee          #+#    #+#             */
-/*   Updated: 2024/09/25 14:54:12 by luvallee         ###   ########.fr       */
+/*   Updated: 2024/10/02 22:19:46 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,49 +33,3 @@ void	add_env_list(t_env_list **list, t_env_list *new)
 		last->next = new;
 	}
 }
-
-// static t_env_list	*create_node_env_list(t_env_list *env_list, char *name,
-// 		char *data)
-// {
-// 	t_env_list	*new;
-
-// 	if (!name || !data)
-// 		return (NULL);
-// 	new = malloc(sizeof(t_env_list));
-// 	if (!new)
-// 		return (printf("create_node_env: allocation error\n"), NULL);
-// 	new->name = ft_strdup(name);
-// 	if (!new->name)
-// 	{
-// 		free(new);
-// 		return (printf("create_node_env: allocation error\n"), NULL);
-// 	}
-// 	new->data = ft_strdup(data);
-// 	if (!new->data)
-// 	{
-// 		free(new);
-// 		free(new->name);
-// 		return (printf("create_node_env: allocation error\n"), NULL);
-// 	}
-// 	add_env_list(&env_list, new);
-// 	return (env_list);
-// }
-
-// t_env_list	*no_env_var(void)
-// {
-// 	t_env_list	*env_list;
-// 	char		buffer[1024];
-
-// 	env_list = malloc(sizeof(t_env_list));
-// 	if (!env_list)
-// 		return (printf("no_env_var.0: allocation error\n"), NULL);
-// 	env_list->name = ft_strdup("PWD");
-// 	env_list->data = ft_strdup(getcwd(buffer, 1024));
-// 	if (!env_list->name || !env_list->data)
-// 		return (free_env_list(env_list));
-// 	if (!create_node_env_list(env_list, "SHLVL", getenv("SHLVL")))
-// 		return (printf("no_env_var.1: allocation error\n"), free_env_list(env_list));
-// 	if (!create_node_env_list(env_list, "_", getenv("_")))
-// 		return (printf("no_env_var.2: allocation error\n"), free_env_list(env_list));
-// 	return (env_list);
-// }
